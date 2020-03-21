9d090d95d9ad677d39be4aa08c3bb56ea1cbc657
rroldan <roldan.ricardo@gmail.com>
2020-03-21 09:23:01 +0100

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {SessionGuard} from './guards/session/session.guard';
 import {ApiService} from './services/api/api.service';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -37,7 +38,8 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
             }
         }),
         HomePageModule,
-        AccessPageModule
+        AccessPageModule,
+        BrowserAnimationsModule
     ],
     providers: [
         ApiService,
